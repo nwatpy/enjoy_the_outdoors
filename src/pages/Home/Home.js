457171d7f41fad5092f2507bb841484207f2c348
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-19 15:24:21 -0400

    Mountains drop down

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,11 @@
 import "../../App.css";
-import MountainImg from "../../assets/images/mountains/Adams-StoryImage_2.jpg"
 import Container from "react-bootstrap/Container";
 import Image from 'react-bootstrap/Image';
 
 function Home() {
   return <Container className="pt-2">
     
-    <Image src={MountainImg} fluid /><br></br>
+    <Image src={process.env.PUBLIC_URL + "/images/mountains/Adams-StoryImage_2.jpg"} className="mountain-hero-image" fluid /><br></br>
     This is where we are going to put something. GO to the moutains, dont waste your time importing images.
 
   </Container>;
--- src/pages/Mountains/Mountains.js
+++ src/pages/Mountains/Mountains.js
@@ -1,27 +1,41 @@
+import { useState } from 'react';
 import "../../App.css";
 import "./Mountains.css";
 import Form from 'react-bootstrap/Form';
-import MountainImg from "../../assets/images/mountains/Liberty-StoryImage_2.jpg";
 import Image from 'react-bootstrap/Image';
 import mountains from "../../assets/data/mountains.json";
 
 function Mountains() {
-  console.log(mountains)
+  const [selectedMountain, setSelectedMountain] = useState(null);
+
+  const handleMountainChange = (e) => {
+    const mountain = mountains.mountains.find(mtn => mtn.name === e.target.value);
+    setSelectedMountain(mountain);
+  }
+
   return (
     <div className="Mountain">
-    <Image src={MountainImg} className="mountain-hero-image" fluid /><br></br>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
+      {selectedMountain && 
+        <Image src={process.env.PUBLIC_URL + "/images/mountains/Adams-StoryImage_2.jpg"} className="mountain-hero-image" fluid />
+      }
+      <br></br>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Select a mountain</Form.Label>
-      <Form.Select aria-label="Default select example">
-        <option>Open this select menu</option>
-        { mountains.mountains.map((mountain) => {
-          return (
-            <option value={mountain.name}>{mountain.name}</option>
-          )
-        })}
-         <option value="nh">gsdgsdgf</option>
-      </Form.Select>
-    </Form.Group>
+        <Form.Select aria-label="Default select example" onChange={handleMountainChange}>
+          <option>Open this select menu</option>
+          {mountains.mountains.map((mountain) => {
+            return (
+              <option key={mountain.name} value={mountain.name}>{mountain.name}</option>
+            )
+          })}
+        </Form.Select>
+      </Form.Group>
+      {selectedMountain && 
+        <div>
+          <h2>{selectedMountain.name}</h2>
+          <p>{selectedMountain.description}</p>
+        </div>
+      }
     </div>
   );
 }

5a96d76a52612663c099e9e75b77a71ea2a0fb09
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-20 11:04:34 -0400

    making mountaincard prettier

--- src/pages/Mountains/MountainCard.js
+++ src/pages/Mountains/MountainCard.js
@@ -1,12 +1,15 @@
 import Card from 'react-bootstrap/Card';
 
-function MountainCard({img, desc}) {
+function MountainCard({img, desc, elevation, effort, coords}) {
   return (
     <>
       <Card>
         <Card.Body>
           <Card.Text>
-           {desc}
+           {desc} <br/>
+           <strong>Elevation:</strong> {elevation} feet <br/>
+           <strong>Effort Required:</strong> {effort} <br/>
+           <strong>Latitude:</strong> {coords.lat}, <strong>Longitude:</strong> {coords.lng}
           </Card.Text>
         </Card.Body>
         <Card.Img variant="bottom" src={process.env.PUBLIC_URL + "/images/mountains/" + img} />

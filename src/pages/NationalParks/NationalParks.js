4b1c448c7cc985d401ee0eff486a107977f8e69c
Bryce Schultz <bryceschultz26@gmail.com>
2023-06-20 11:28:21 -0400

    adding filter dropdowns for nationalparks page

--- src/pages/NationalParks/NationalParks.js
+++ src/pages/NationalParks/NationalParks.js
@@ -1,13 +1,63 @@
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import locations from "../../assets/data/locations.json";
+import parktypes from "../../assets/data/parktypes.json";
 import "../../App.css";
-import Container from "react-bootstrap/Container";
-import Image from 'react-bootstrap/Image';
+import "./NationalParks.css";
 
 function NationalParks() {
+    const [filterBy, setFilterBy] = useState("Location");
+    const [location, setLocation] = useState(null);
+    const [parkType, setParkType] = useState(null);
+    const handleFilterByChange = (e) => {
+        setFilterBy(e.target.value);
+    }
+    const handleLocationChange = (e) => {
+        setLocation(e.target.value);
+    }
+    const handleParkTypeChange = (e) => {
+        setParkType(e.target.value);
+    }
+
+    console.log(filterBy)
+    console.log(location)
+    console.log(parkType)
     return (
-        <Container className="pt-2">
-            <Image src={process.env.PUBLIC_URL + "/images/mountains/Adams-StoryImage_2.jpg"} className="mountain-hero-image" fluid /><br></br>
-            This is where we are going to put something. GO to the moutains, dont waste your time importing images.
-        </Container>
+        <div className="National-Parks">
+            <br></br>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Filter by:</Form.Label>
+                <Form.Select aria-label="Default select example" onChange={handleFilterByChange}>
+                    <option key="Location" value="Location">Location</option>
+                    <option key="Park Type" value="Park Type">Park Type</option>
+                </Form.Select>
+                {(filterBy === "Location") ? (
+                    <>
+                        <Form.Label>Select a location</Form.Label>
+                        <Form.Select aria-label="Default select example" onChange={handleLocationChange}>
+                            <option>Open this select menu</option>
+                            {locations.map((location) => {
+                                return (
+                                    <option key={location} value={location}>{location}</option>
+                                )
+                            })}
+                        </Form.Select>
+                    </>
+                ) : (
+                    <>
+                        <Form.Label>Select a park type</Form.Label>
+                        <Form.Select aria-label="Default select example" onChange={handleParkTypeChange}>
+                            <option>Open this select menu</option>
+                            {parktypes.map((parktype) => {
+                                return (
+                                    <option key={parktype} value={parktype}>{parktype}</option>
+                                )
+                            })}
+                        </Form.Select>
+                    </>
+                )}
+            </Form.Group>
+        </div>
     )
 }
 

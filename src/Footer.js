b85dbfca83e57b7a976c54a653c7a004d7672441
n0119566 <cadzima.lm@gmail.com>
2023-06-20 09:28:21 -0400

    Fix header formatting and remove unecessary imports in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 function Footer() {
-    const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
   return (
     <Navbar bg="primary" data-bs-theme="dark">
-    <Container>
-      <Navbar.Brand href="#home">&copy;Enjoy the Outdoors - {year} </Navbar.Brand>
-    </Container>
-  </Navbar>
+      <Container>
+        <Navbar.Brand href="#home">
+          &copy;Enjoy the Outdoors - {year}{" "}
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,27 @@
 import "./App.css";
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   return (
     <Navbar bg="primary" data-bs-theme="dark">
-    <Container>
-      <Navbar.Brand href="#home">Enjoy the Outdoors</Navbar.Brand>
-      <Nav className="me-auto">
-      <Link to="/">Home</Link>
-      <Link to="/nationalparks"> National Parks </Link>
-      <Link to="/mountains"> Mountains </Link>
-      </Nav>
-    </Container>
-  </Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Enjoy the Outdoors</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link>
+            <Link to="/">Home</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/nationalparks">National Parks</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/mountains">Mountains</Link>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 

9bd5ea686caf32699ca3c7006aeff5625e703f5e
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-20 13:38:24 -0400

    mountain-banner image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ import Image from 'react-bootstrap/Image';
 function Home() {
   return <Container className="pt-2">
     
-    <Image src={process.env.PUBLIC_URL + "/images/mountains/Adams-StoryImage_2.jpg"} className="mountain-hero-image" fluid /><br></br>
+    <Image src={process.env.PUBLIC_URL + "/images/mountain-banner.jpeg"} className="mountain-hero-image"/><br></br>
     This is where we are going to put something. GO to the moutains, dont waste your time importing images.
 
   </Container>;

f34113223b8ef4a6762e30dc8027fff66b9039b4
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-21 13:36:20 -0400

    Fixing footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ import Navbar from "react-bootstrap/Navbar";
 
 function Footer() {
   return (
-    <Navbar bg="primary" data-bs-theme="dark">
+    <Navbar bg="primary" variant="dark" fixed="bottom">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand>
           &copy;Enjoy the Outdoors - {new Date().getFullYear()}
         </Navbar.Brand>
       </Container>

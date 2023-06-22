5bee21814a206fa0cbd7689ff1bb249a73cfe0de
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-22 11:04:54 -0400

    refactor secrets

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const getParkImage = async (parkCode) => {
-    const apiKey = "el1exbrk6jVfffHnExYVFyYGOTGRzjprAHoKpQ1d";
-    const res = await axios.get(`https://developer.nps.gov/api/v1/multimedia/galleries/assets?parkCode=${parkCode}&api_key=${apiKey}&limit=1`);
+    const NATIONALPARKSAPI = process.env.NATIONALPARKSAPI;
+    const res = await axios.get(`https://developer.nps.gov/api/v1/multimedia/galleries/assets?parkCode=${parkCode}&api_key=${NATIONALPARKSAPI}&limit=1`);
     if (res.status === 200) {
         return res.data.data[0]?.fileInfo.url;
     }

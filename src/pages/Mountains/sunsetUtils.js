71cb42ed090421c817ea287709d1ca02a6521c1d
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-22 19:17:45 -0400

    fixing https protocol on api request

--- src/pages/Mountains/sunsetUtils.js
+++ src/pages/Mountains/sunsetUtils.js
@@ -1,5 +1,5 @@
 export async function getSunsetForMountain(coords) {
-    let response = await fetch(`http://api.sunrise-sunset.org/json?lat=${coords.lat}&lng=${coords.lng}&date=today`)
+    let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${coords.lat}&lng=${coords.lng}&date=today`)
     let data = await response.json()
     return data.results
   }

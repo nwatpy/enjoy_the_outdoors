1bcc69beca4dbf510779d6fcdd43d6970c448167
n0119566 <cadzima.lm@gmail.com>
2023-06-22 10:58:05 -0400

    Framework for park info page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Mountains from "./pages/Mountains/Mountains";
 import SkiFree from "./pages/SkiFree/SkiFreeGame";
 import Footer from "./Footer";
 import Header from "./Header";
+import ParkInfo from "./pages/ParkInfo/ParkInfo";
 import mountainList from "./assets/data/mountains.json";
 import parklist from "./assets/data/nationalparks.json";
 import getParkImage from "./utils";
@@ -51,6 +52,7 @@ function App() {
           <Route path="/nationalparks" element={<NationalParks parksToDisplay={parksToDisplay} />} />
           <Route path="/mountains" element={<Mountains mountainsToDisplay={mountainsToDisplay} />} />
           <Route path="/skifree" element={<SkiFree />} />
+          <Route path="/parkinfo/:locationID" element={<ParkInfo />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/pages/ParkInfo/ParkInfo.js
@@ -0,0 +1,36 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useState } from "react";
+import Loading from "../../Loading";
+import { useParams } from "react-router-dom";
+
+function ParkInfo() {
+  const { locationID } = useParams();
+
+  
+
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Body>
+        <Card.Title>{locationID}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ParkInfo;

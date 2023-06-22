547a1792c54ce2878c24428c907ce1aa6ad3abe9
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-22 11:38:15 -0400

    secrets and react github deployment

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getParkImage = async (parkCode) => {
-    const NATIONALPARKSAPI = process.env.NATIONALPARKSAPI;
+    REACT_APP_NATIONALPARKSAPI=${{ secrets.NATIONALPARKSAPI }}
     const res = await axios.get(`https://developer.nps.gov/api/v1/multimedia/galleries/assets?parkCode=${parkCode}&api_key=${NATIONALPARKSAPI}&limit=1`);
     if (res.status === 200) {
         return res.data.data[0]?.fileInfo.url;

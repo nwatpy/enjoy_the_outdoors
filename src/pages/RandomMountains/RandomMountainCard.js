2824038da048b89fee1e687037f744717f8764ce
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-22 10:12:36 -0400

    adding sunrise/sunset API to RandomMountainCards

--- src/pages/RandomMountains/RandomMountainCard.js
+++ src/pages/RandomMountains/RandomMountainCard.js
@@ -1,15 +1,36 @@
+import React from "react";
 import "./RandomMountains.css";
 import Card from "react-bootstrap/Card";
+import { getSunsetForMountain } from "../Mountains/sunsetUtils.js";
 
-function RandomMountainCard({ name, desc, img }) {
+function RandomMountainCard({ name, desc, img, coords }) {
+  const [sunsetData, setSunsetData] = React.useState(null);
+
+  React.useEffect(() => {
+    getSunsetForMountain(coords)
+      .then(data => {
+        setSunsetData(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [coords]);
 
   return (
     <Card key={name} className="RandomMountainCard">
       <Card.Body>
         <Card.Img variant="bottom" src={process.env.PUBLIC_URL + "/images/mountains/" + img} />
-        <Card.Title className="pt-2">{name}</Card.Title>
+        <br />
+        <Card.Title className="pt-2"><strong>{name}</strong></Card.Title>
         <Card.Text>
-          {desc}
+          {desc}<br />
+          <br />
+          {sunsetData && (
+              <>
+                <strong>Sunrise: {sunsetData.sunrise} <br/>
+                Sunset: {sunsetData.sunset}</strong> <br/>
+              </>
+            )}
         </Card.Text>
       </Card.Body>
     </Card>

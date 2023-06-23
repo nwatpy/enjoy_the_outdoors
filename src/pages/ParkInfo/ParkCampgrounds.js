64a10316a5671435e44cffefd2ab032bf19cb0be
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-22 22:35:10 -0400

    fixing image error on parkcampgrounds.js

--- src/pages/ParkInfo/ParkCampgrounds.js
+++ src/pages/ParkInfo/ParkCampgrounds.js
@@ -7,15 +7,16 @@ function ParkCampgrounds(locationID) {
   const [campgrounds, setCampgrounds] = useState([]);
 
   useEffect(() => {
-    const getCampgrounds = async () => {
-      const campgrounds = await getParkCampgrounds(locationID);
-      setCampgrounds(campgrounds);
-    };
-  
     if (locationID) {
       getCampgrounds();
     }
   }, [locationID]);
+
+  const getCampgrounds = async () => {
+    const campgrounds = await getParkCampgrounds(locationID);
+    setCampgrounds(campgrounds);
+  };
+
 
   return (
     <CardGroup>

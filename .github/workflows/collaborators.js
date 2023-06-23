13be826f047dd36895ff38dadbe6149018720383
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-23 13:07:40 -0400

    collaborators automation

---
+++ .github/workflows/collaborators.js
@@ -0,0 +1,20 @@
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+});
+
+const owner = "nwatpy";
+const repo = "enjoy_the_outdoors";
+
+octokit.repos.listCollaborators({ owner, repo })
+  .then(response => {
+    const collaboratorUsernames = response.data.map(collaborator => collaborator.login);
+    const collaboratorList = collaboratorUsernames.join("\n");
+    const collaboratorFileContents = `Collaborators:\n\n${collaboratorList}`;
+ 
+    require("fs").writeFileSync("COLLABORATORS", collaboratorFileContents, "utf8");
+  })
+  .catch(error => {
+    console.error(error);
+  });

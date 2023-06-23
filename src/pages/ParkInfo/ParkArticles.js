4e87d90f76751017701a6b603acc9c8c5c2ac488
nwatpy <nicholas.waterman@libertymutual.com>
2023-06-22 21:42:33 -0400

    continued code refactor to satisfy GitHub Actions tyranny

--- src/pages/ParkInfo/ParkArticles.js
+++ src/pages/ParkInfo/ParkArticles.js
@@ -7,15 +7,15 @@ function ParkArticles(locationID) {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
+    const getArticles = async () => {
+      const campgrounds = await getParkArticles(locationID, 3);
+      setArticles(campgrounds);
+    };
+
     if (locationID) {
       getArticles();
     } 
-  }, [locationID, getArticles]);
-
-  const getArticles = async () => {
-    const campgrounds = await getParkArticles(locationID, 3);
-    setArticles(campgrounds);
-  };
+  }, [locationID]);
 
   return (
     <CardGroup>
@@ -25,11 +25,10 @@ function ParkArticles(locationID) {
             <Card.Img variant="top" src={article.listingImage?.url} />
             <Card.Body>
               <Card.Title>{article.title}</Card.Title>
-      
-              <Card.Text>
-                {article.listingDescription}
-              </Card.Text>
-              <Card.Link target="_blank" href={article.url}>Article Link</Card.Link>
+              <Card.Text>{article.listingDescription}</Card.Text>
+              <Card.Link target="_blank" href={article.url}>
+                Article Link
+              </Card.Link>
             </Card.Body>
           </Card>
         ))}
